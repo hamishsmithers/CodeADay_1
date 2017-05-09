36a2b539a5ded29c4290df027ceafd8c5ef623cb
HamishSmithers <hamish.e.smithers@gmail.com>
2017-05-09 17:26:32 +1000

    FirstAttempt At Arrow

--- CodeADay_Arrow/Assets/Scripts/ArrowController.cs
+++ CodeADay_Arrow/Assets/Scripts/ArrowController.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class ArrowController : MonoBehaviour {
 
 	public float speed;
+	Rigidbody myRB;
 
 	// Use this for initialization
 	void Start () {
-		
+		myRB = GetComponent<Rigidbody> ();
+		myRB.AddForce (transform.up * speed);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.Translate ((Vector3.up) * speed/10);
+		transform.up = myRB.velocity;
 	}
+
+//	void OnCollisionEnter(GameObject other){
+//		if(other.tag)
+//	}
 }
